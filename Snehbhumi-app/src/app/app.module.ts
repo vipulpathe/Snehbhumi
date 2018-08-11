1394973510bb2fc98e4b7a384c163e4d013e8a58
vipulpathe <vipulpathe@gmail.com>
2018-08-11 12:30:51 +0530

    login page
    login page

--- Snehbhumi-app/src/app/app.module.ts
+++ Snehbhumi-app/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{ path: '', component: LoginComponent }, { path: 'home', component: HomeComponent }];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LoginComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes, { useHash: true })],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
